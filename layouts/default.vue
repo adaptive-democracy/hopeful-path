178f47da9d54cd71bf8e2e5363eda55f849c5027
Blaine Hansen <faichenshing@gmail.com>
2021-05-24 19:54:30 -0600

    moved contribution button to bottom

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,18 +4,22 @@ div
 	nav.w-full.border-b.border-gray-200
 		.container.py-4.md_py-0.flex.flex-col.md_flex-row.md_h-16.items-center
 			NuxtLink.no-underline.text-xl.mr-auto(to="/") A Hopeful Pathway for Humanity
-			a.hidden.md_block.no-underline(href="https://github.com/blainehansen/hopeful-pathway") Contribute
 
 	Nuxt.pt-6.container
 
 	hr.mt-8
-	section.container.pt-10.pb-12
+	section.container.py-10
 		h3.text-2xl.text-gray-400.mb-4 Table of Contents
 
 		NuxtLink.toc-link.no-underline(v-for="chapter in chapters", :to="chapter.slug", :key="chapter.slug")
 			.mb-2.btn.p-4
 				span.underline {{ chapter.title }}
 				//- span.text-gray-400 &nbsp;- {{ chapter.subtitle }}
+	section.container.pb-12
+		a.no-underline.flex.justify-end(href="https://github.com/blainehansen/hopeful-pathway/blob/main/CONTRIBUTING.md")
+			.btn.flex.items-center
+				Pencil.w-6.h-6
+				span.font-medium.ml-2 Make a contribution
 
 </template>
 
