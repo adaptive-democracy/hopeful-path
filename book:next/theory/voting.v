5a376e5bad8baf303992275be88b75a8ff65fc41
Blaine Hansen <faichenshing@gmail.com>
2022-01-16 21:22:27 -0700

    adding persistent funding to new democracy chapter, moving user defined organization stuff to cooperatives chapter

---
+++ book:next/theory/voting.v
@@ -0,0 +1,13 @@
+(*
+It seems that the perfectly optimal system would be the omniscient mediator
+basically a system capable of communicating with us instantly, who knows our welfare wishes perfectly and knows exactly what we're about to do, and whenever we're about to do something that will interfere with another person it warns us, helps us instantly negotiate with that person to find actions for the both of us that best preserve our welfare intent while minimally interfering with one another.
+this thing doesn't even necessarily have to have perfect knowledge of our internal state, but we can use some sort of weighted vote system to protect certain areas of concern and choose to move them around at this instantaneous level
+this is getting at the fact that the *entire* medium of interaction between welfare-concerned beings is the universe. this means that negotiating for usage capability of different spaces of the universe is the *only* way we need to negotiate with one another. negotiating over actions is really just negotating over *contextualized* actions in relation to the current state of all concerned beings
+
+such a system obviously doesn't exist, probably won't ever exist, even if it could many of us generally dislike the idea of being so closely watched so the very presence of that system would harm our welfare.
+
+so instead we create systems of rights, which are basically a way to *durably* encode space-usage negotiations with a minimum of coordination cost. we negotiate over the *general* structure of these rights systems in our constitutions, and this negotiation represents our best guesses at what kinds of votes we would cast in different kinds of negotiations. for example a society that agrees no one should be able to demand non-interfering actions from others would encode ownership of body, etc.
+
+we also can create rules for more dynamic negotations, ones where we don't think we can make broad rules. for example a system like common resource taxes on land encodes a negotiation algorithm for all physical spaces that all members of the group can use to dynamically adjust ownership according to highest valuation.
+
+*)
